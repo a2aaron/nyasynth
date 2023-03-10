0ed5b55ee12321b040199d3d7b585b540429a5cc
Aaron Kofsky <aaronko@umich.edu>
2023-03-10 15:25:01 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,6 @@
 # ps aux | rg "vPlayer 3"
 
 trash $1.trace
+trash $2.wav
 cargo build --release
-xctrace record --template 'Time Profiler' --output $1.trace --launch -- target/release/perf --in megalovania.mid --out $2.wav --polycat --vst target/release/libnyasynth.dylib
+xctrace record --template 'Time Profiler' --output $1.trace --launch -- target/release/perf --in megalovania.mid --out $2.wav --polycat
