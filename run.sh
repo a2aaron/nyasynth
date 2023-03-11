ae6e52303f57f123496791e23bf3869d2f719288
Aaron Kofsky <aaronko@umich.edu>
2023-03-10 20:55:27 -0500

    Build the vst3 bundle when running run.sh

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@
 trash $1.trace
 trash $2.wav
 cargo build --release
+cargo xtask bundle nyasynth --release
 xctrace record --template 'Time Profiler' --output $1.trace --launch -- target/release/perf --in megalovania.mid --out $2.wav --polycat
